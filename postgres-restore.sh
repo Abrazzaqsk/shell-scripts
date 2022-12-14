c6ca105c80821d5f1a1d3dd5a6e3924071b0b397
Abdul Razzaq Shaikh <80508244+Abrazzaqsk@users.noreply.github.com>
2022-12-14 16:08:13 +0530

    Update postgres-restore.sh

--- postgres-restore.sh
+++ postgres-restore.sh
@@ -3,9 +3,12 @@ exec > /var/lib/pgsql/nissan_configuration-$(date +%d-%m-%Y).log
 echo "go to postgres default location"
 cd /var/lib/pgsql/
 
-echo "pulling backup dump"
+echo "pulling backup dump from s3 "
 aws s3 cp s3://nissan-prod-db-backup/nissan_configuration-$(date +%d-%m-%Y).sql nissan_configuration-$(date +%d-%m-%Y).sql
-ech "pull done"
+echo "pull done"
+
+echo "terminate existing connections"
+PGPASSWORD=Network@18 psql -U postgres -c "SELECT pg_terminate_backend(pg_stat_activity.pid)FROM pg_stat_activity WHERE pg_stat_activity.datname = 'stg_nissanreports';"
 
 echo "DROP Exiting database"
 PGPASSWORD=Network@18 psql -U postgres -c 'DROP DATABASE IF EXISTS stg_nissanreports;'
@@ -15,7 +18,11 @@ echo "create database"
 PGPASSWORD=Network@18 psql -U postgres -c 'create database stg_nissanreports;'
 echo "creation database done"
 
-
 echo "restore database"
 PGPASSWORD=Network@18 psql -U postgres stg_nissanreports < nissan_configuration-$(date +%d-%m-%Y).sql
 echo "restore database completed "
+
+
+echo "delete from local server"
+rm -rf nissan_configuration-$(date +%d-%m-%Y).sql
+echo "delete from server done"
