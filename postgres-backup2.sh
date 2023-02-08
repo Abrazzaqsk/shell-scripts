d28df700346d0898b4eb2893af57be6e72afcacf
Abdul Razzaq Shaikh <80508244+Abrazzaqsk@users.noreply.github.com>
2023-02-08 10:58:56 +0530

    Update postgres-backup2.sh

--- postgres-backup2.sh
+++ postgres-backup2.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-sudo exec > /home/devuser/nissan-api/file/nissan_configuration-logs$(date +%d-%m-%Y).log
+sudo exec > /home/file/DBNAME-logs$(date +%d-%m-%Y).log
 
 echo "backup start"
-sudo PGPASSWORD=5J8QZZmuqTB2Zbx5 pg_dump -U postgres -h nmipl-magnite-proddb.c9ijqfqikutm.ap-south-1.rds.amazonaws.com -p 5432 nissan_configuration > nissan_configuration-$(date +%d-%m-%Y).sql
+sudo PGPASSWORD=Password pg_dump -U postgres -h RDS-ENDPOINT -p 5432 DBNAME > DBNAME-$(date +%d-%m-%Y).sql
 echo "backup done"
 
 echo "upload on s3"
-sudo /usr/local/bin/aws s3 cp nissan_configuration-$(date +%d-%m-%Y).sql s3://nissan-prod-db-backup
+sudo /usr/local/bin/aws s3 cp DBNAME-$(date +%d-%m-%Y).sql s3://prod-db-backup
 echo "upload done"
 
 echo "delete from local server"
-#rm -rf nissan_configuration-$(date +%d-%m-%Y).sql
+#rm -rf DBANME-$(date +%d-%m-%Y).sql
 echo "delete done"
