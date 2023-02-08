1a97aee3e46a80c271f1ccd9985e039663bf4e55
Abdul Razzaq Shaikh <80508244+Abrazzaqsk@users.noreply.github.com>
2023-02-08 11:02:39 +0530

    Update postgres-restore.sh

--- postgres-restore.sh
+++ postgres-restore.sh
@@ -1,28 +1,28 @@
 #!/bin/bash PostgrSQL 12 Supported 
-exec > /var/lib/pgsql/nissan_configuration-$(date +%d-%m-%Y).log
+exec > /var/lib/pgsql/DBNAME-$(date +%d-%m-%Y).log
 echo "go to postgres default location"
 cd /var/lib/pgsql/
 
 echo "pulling backup dump from s3 "
-aws s3 cp s3://nissan-prod-db-backup/nissan_configuration-$(date +%d-%m-%Y).sql nissan_configuration-$(date +%d-%m-%Y).sql
+aws s3 cp s3://prod-db-backup/DBNAME-$(date +%d-%m-%Y).sql DBNAME-$(date +%d-%m-%Y).sql
 echo "pull done"
 
 echo "terminate existing connections"
-PGPASSWORD=Network@18 psql -U postgres -c "SELECT pg_terminate_backend(pg_stat_activity.pid)FROM pg_stat_activity WHERE pg_stat_activity.datname = 'stg_nissanreports';"
+PGPASSWORD=PASSWORD-OF_DB psql -U postgres -c "SELECT pg_terminate_backend(pg_stat_activity.pid)FROM pg_stat_activity WHERE pg_stat_activity.datname = 'stg_reports';"
 
 echo "DROP Exiting database"
-PGPASSWORD=Network@18 psql -U postgres -c 'DROP DATABASE IF EXISTS stg_nissanreports;'
+PGPASSWORD=PASSWORD psql -U postgres -c 'DROP DATABASE IF EXISTS DBNAME;'
 echo "Droping done"
 
 echo "create database"
-PGPASSWORD=Network@18 psql -U postgres -c 'create database stg_nissanreports;'
+PGPASSWORD=PASSWORD psql -U postgres -c 'create database DBNAME;'
 echo "creation database done"
 
 echo "restore database"
-PGPASSWORD=Network@18 psql -U postgres stg_nissanreports < nissan_configuration-$(date +%d-%m-%Y).sql
+PGPASSWORD=PASSWORD psql -U postgres DBNAME < DBNAME-$(date +%d-%m-%Y).sql
 echo "restore database completed "
 
 
 echo "delete from local server"
-rm -rf nissan_configuration-$(date +%d-%m-%Y).sql
+rm -rf DBNAME-$(date +%d-%m-%Y).sql
 echo "delete from server done"
